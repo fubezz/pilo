1f96687d4ec28cb3c627b643c9c1a82898417678
Fubezz <fubezz@gmail.com>
2021-02-18 22:41:21 +0100

    working arduino script

--- arduino/programm.ino
+++ arduino/programm.ino
@@ -1,20 +1,25 @@
 
 #include <Keyboard.h>
 
-
 void setup(){
   Serial1.begin(9600);
+  Keyboard.begin();
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, 0);
 }
 
 void loop(){
-  while (Serial1.available()) {
-    byte x = Serial1.read();
-    digitalWrite(LED_BUILTIN, 1);
-    delay(10);
-    digitalWrite(LED_BUILTIN, 0);
-    Serial1.write(x);
+  while (Serial1.available() > 1) {
+    byte msg[2] = {0, 0};
+    Serial1.readBytes(msg,2);
+    if (msg[0] == 0x01){
+      digitalWrite(LED_BUILTIN, 1);
+      delay(10);
+      digitalWrite(LED_BUILTIN, 0);
+      Keyboard.press(msg[1]); 
+    }else{
+      Keyboard.release(msg[1]);
+    }
   } 
   delay(100);
 }
